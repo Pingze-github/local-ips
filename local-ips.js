16f07e965cfc9c885499b4d19be86431cc1d35fe
pingze <719695274@qq.com>
2017-01-04 13:39:15 +0800

    last commit forget to add loadSync()

--- local-ips.js
+++ local-ips.js
@@ -2,15 +2,17 @@
 
 var fs = require('fs');
 
-var locations = ['北京','天津','河北','山西','内蒙古','辽宁','吉林','黑龙江','上海','江苏','浙江','安徽','福建','江西','山东','河南','湖北','湖南','广东','广西','海南','重庆','四川','贵州','云南','西藏','陕西','甘肃','青海','宁夏','新疆','国内'];
+var json_path = './local-ips.json';
+
+var locations = ['北京','天津','河北','山西','内蒙古','辽宁','吉林','黑龙江','上海','江苏','浙江','安徽','福建','江西','山东','河南','湖北','湖南','广东','广西','海南','重庆','四川','贵州','云南','西藏','陕西','甘肃','青海','宁夏','新疆'];
 
 exports.load = function(location,callback){
-    fs.readFile('./local-ips.json','utf-8', function(err,json){
+    fs.readFile(json_path,'utf-8', function(err,json){
         if(err){
             throw err;
         }else{
             var data = JSON.parse(json);
-            if (province_list.indexOf(location)>-1){
+            if (locations.indexOf(location)>-1){
                 callback(data[location]);
             }else if(location=="国内"){
                 var data_all = [];
@@ -23,11 +25,32 @@ exports.load = function(location,callback){
     })
 }
 
+exports.loadSync = function(location){
+    console.log(location)
+    var json = fs.readFileSync(json_path,'utf-8');
+    var data = JSON.parse(json);
+    if (locations.indexOf(location)>-1){
+        return(data[location]);
+    }else if(location=="国内"){
+        var data_all = [];
+        for (var key in data){
+            data_all = data_all.concat(data[key]);
+            console.log(data_all);
+        }
+        return(data_all);
+    }else{
+        console.log("[local-ips] 查询位置不在列表中")
+    }
+}
+
 exports.locations = function(){
-    return locations;
+    var locations_all = locations.slice();
+    locations_all.push("国内");
+    return locations_all;
 }
 
 //test
-/*exports.load("国内",function(list){
-    console.log(list)
+/*exports.load("上海",function(list){
+    console.log(list);
 });*/
+/*console.log(exports.loadSync("国内"));*/
